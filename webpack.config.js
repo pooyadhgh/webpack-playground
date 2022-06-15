69977a5271f467f173db90ae828dd163d28daa6b
pooyadhgh <pooya.dhgh@gmail.com>
2022-06-15 21:45:49 +0200

    Config asset module for images

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,20 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist',
   },
   mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/i,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024, // 3KB
+          },
+        },
+      },
+    ],
+  },
 };
