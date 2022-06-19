99a3b62b36f3164aa99d49e473a7e33e12116682
Pooya Dehghani <pooya.dehghani@hm.com>
2022-06-19 09:30:58 +0200

    Add header component

---
+++ src/components/Header/header.js
@@ -0,0 +1,21 @@
+import "./header.scss";
+
+class Header {
+  render() {
+    const body = document.querySelector("body");
+    const header = document.createElement("div");
+    header.innerHTML = `
+    <h1>Hello World</h1>
+    <nav>
+        <ul>
+            <li><a href='/'>Home Page</a></li>
+            <li><a href='/about.html'>About Page</a></li>
+        </ul>   
+    </nav>
+    `;
+    header.classList.add("hello-world-header");
+    body.appendChild(header);
+  }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import Button from './components/button';
+import Header from "./components/Header/header";
+import Button from "./components/Button/button";
 
+const header = new Header();
 const button = new Button();
+header.render();
 button.render();
