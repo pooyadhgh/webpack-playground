ac435fb5dba5a8fa8d5e7cf89fe2741ff3fbe935
Pooya Dehghani <pooya.dehghani@hm.com>
2022-06-19 12:42:56 +0200

    Consume federated modules

--- hello-another-world/src/pages/index.js
+++ hello-another-world/src/pages/index.js
@@ -2,3 +2,9 @@ import Header from '../components/Header/header';
 
 const header = new Header();
 header.render();
+
+import('HelloWorldApp/HelloWorldButton').then((HelloWorldButtonModule) => {
+  const HelloWorldButton = HelloWorldButtonModule.default;
+  const button = new HelloWorldButton();
+  button.render();
+});
